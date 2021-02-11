4e06e537fe331a6f36936aac1998f95daaa7dbf2
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-11 16:05:08 +0100

    create textcopy.txt

--- Pattern Recognition and Regular expression/phone_and_email_exctractor.py
+++ Pattern Recognition and Regular expression/phone_and_email_exctractor.py
@@ -11,8 +11,8 @@ phone_num_regex = re.compile(r'''(
     (\d{4})                     #last 4 digits
 )''', re.VERBOSE)
 
-mo = phone_num_regex.search("+234-813-581-0804")
-print(mo.group())
+#mo = phone_num_regex.search("+234-813-581-0804")
+#print(mo.group())
 
 # creating an email regex
 email_regex = re.compile(r'''(
@@ -22,8 +22,27 @@ email_regex = re.compile(r'''(
     (\.[a-zA-Z]{2,4})           #dot-something
 )''', re.VERBOSE)
 
-mo1 = email_regex.search("musawakiliml1@gmail.com")
-print(mo1)
+#mo1 = email_regex.search("musawakiliml1@gmail.com")
+#print(mo1)
 
 # finding match in clipboard text.
-text = pyperclip.paste()
+text = str(pyperclip.paste())
+
+matches = [] # creating an empty list
+# finding all the matching phone number
+for groups in phone_num_regex.findall(text):
+    phone_num_regex = '-'.join([groups[1],groups[3],groups[5],groups[7]])
+    matches.append(phone_num_regex)
+
+# finding all the matching email address
+for groups in email_regex.findall(text):
+    matches.append(groups[0])
+
+# copy result to the clipboard
+if len(matches) > 0:
+    pyperclip.copy("\n".join(matches))
+    print("Copied to clipboard")
+    print("\n".join(matches))
+else:
+    print("No phone number of email found")
+
