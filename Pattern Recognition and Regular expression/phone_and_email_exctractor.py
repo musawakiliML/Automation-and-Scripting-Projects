23d667cffa058510c9da0dd7efdf72f363135744
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-11 16:08:44 +0100

    Update phone_and_email_exctractor.py

--- Pattern Recognition and Regular expression/phone_and_email_exctractor.py
+++ Pattern Recognition and Regular expression/phone_and_email_exctractor.py
@@ -1,6 +1,8 @@
+# regular expression and clipboard library
 import re, pyperclip
 # create a phone number regex
 phone_num_regex = re.compile(r'''(
+
     [+]?                         #+ sign for the area code
     (\d{3}|\(\d{3}\))?          #areacode
     (\s|-|\.)?                  #seperator
@@ -9,6 +11,7 @@ phone_num_regex = re.compile(r'''(
     (\d{3})                     #second 3 numbers
     (\s|-|\.)                   #seperator
     (\d{4})                     #last 4 digits
+
 )''', re.VERBOSE)
 
 #mo = phone_num_regex.search("+234-813-581-0804")
@@ -16,10 +19,12 @@ phone_num_regex = re.compile(r'''(
 
 # creating an email regex
 email_regex = re.compile(r'''(
+
     [a-zA-Z0-9._%+-]+           #username
     @                           #@ symbol
     [a-zA-Z0-9.-]+              #domain name
     (\.[a-zA-Z]{2,4})           #dot-something
+
 )''', re.VERBOSE)
 
 #mo1 = email_regex.search("musawakiliml1@gmail.com")
