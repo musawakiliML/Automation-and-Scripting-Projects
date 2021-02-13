ec5f28f402b98bb5043347a01ee1fb6b6b6989a6
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-13 23:44:57 +0100

    Update strong_password.py

--- Pattern Recognition and Regular expression/strong_password.py
+++ Pattern Recognition and Regular expression/strong_password.py
@@ -1,9 +1,9 @@
 import re
 
 strong_password = re.compile(r'''(
-    ([A-z])
-    [a-zA-Z0-9]+           #character class, upper, lower and numbers
-                           #character for symbols
+    ^(?=.*[A-Z].*[A-Z])     # at least two characters
+     (?=.*[!@#$&*])         # at least one of the special characters
+     (?=.*[0-9].*[0-9])     # at least two numeric digits
 
 )''', re.VERBOSE)
 
