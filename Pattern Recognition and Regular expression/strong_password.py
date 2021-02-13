5621735d818089f2d041fd96d785f26be89e1eaf
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-13 23:31:22 +0100

    Update strong_password.py

--- Pattern Recognition and Regular expression/strong_password.py
+++ Pattern Recognition and Regular expression/strong_password.py
@@ -1,9 +1,12 @@
 import re
 
 strong_password = re.compile(r'''(
-    [a-zA-Z0-9]+        #character class, upper, lower and numbers
+
+    [a-zA-Z0-9]+           #character class, upper, lower and numbers
     [@+\%_|-|\.$]+          #character for symbols
+
 )''', re.VERBOSE)
 
-mo = strong_password.search("pro244grammer.")
+
+mo = strong_password.search("pro244grammEr.")
 print(mo.group())
