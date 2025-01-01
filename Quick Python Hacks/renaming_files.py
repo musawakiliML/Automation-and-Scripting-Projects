bac0f5556339e59af31acc97c148996375eeccde
Musa Adamu Wakili <musawakiliml@hotmail.com>
2025-01-01 01:27:38 +0100

    Update: Added renaming_files.py

---
+++ Quick Python Hacks/renaming_files.py
@@ -0,0 +1,11 @@
+import os
+
+def rename_files_in_folder(folder_path, prefix="file"):
+    try:
+        pass
+    except Exception as e:
+        print(f"Error {e}")
+
+
+if __name__ == "__main__":
+    pass
